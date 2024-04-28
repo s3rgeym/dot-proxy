fcbf06265c51b108e7a2998508307d2042cf1c3b
None <yamldeveloper@proton.me>
2024-04-29 00:08:35 +0300

    выбирается случайный DNS-сервер, если оный не указан; переименование в тч флагов

--- dot_proxy.py
+++ dot_proxy.py
@@ -18,50 +18,51 @@ __author__ = "Sergey M"
 
 
 # https://dnsprivacy.org/public_resolvers/
+# На данный момент нельзя создать ssl-соединение с IPv6 адресом
+# FIXME: OSError: [Errno 101] Network is unreachable
 DOT_SERVERS = [
     # quad9
     "9.9.9.9",
-    "2620:fe:0:0:0:0:0:fe",  # "2620:fe::fe",
+    # "2620:fe::fe",
     "9.9.9.10",
-    "2620:fe:0:0:0:0:0:10",  # "2620:fe::10",
+    # "2620:fe::10",
     # cloudflare
     "1.1.1.1",
     "1.0.0.1",
-    "2606:4700:4700:0:0:0:0:1111",  # "2606:4700:4700::1111",
-    "2606:4700:4700:0:0:0:0:1001",  # "2606:4700:4700::1001",
+    # "2606:4700:4700::1111",
+    # "2606:4700:4700::1001",
     # google
     "8.8.8.8",
     "8.8.4.4",
-    "2001:4860:4860:0:0:0:0:8888",  # "2001:4860:4860::8888",
-    "2001:4860:4860:0:0:0:0:8844",  # "2001:4860:4860::8844",
+    # "2001:4860:4860::8888",
+    # "2001:4860:4860::8844",
 ]
 
 DEFAULT_DOT_PORT = 853
 
 
 @dataclass
-class DOTClient:
+class ClientConnection:
     host: str | None = None
     port: int | None = None
     reader: asyncio.StreamReader | None = None
     writer: asyncio.StreamWriter | None = None
 
     async def connect(self: Self) -> None:
-        sslctx = ssl.create_default_context()
+        ctx = ssl.create_default_context()
+        # ctx.check_hostname = False
+        # ctx.verify_mode = ssl.CERT_NONE
+
         host = self.host or secrets.choice(DOT_SERVERS)
         port = self.port or DEFAULT_DOT_PORT
+
         logging.debug("connect to %s#%d", host, port)
 
-        # for fam in (socket.AF_INET, socket.AF_INET6):
-        #     try:
         self.reader, self.writer = await asyncio.open_connection(
             host,
             port,
-            ssl=sslctx,
-            verify_ssl=False,
+            ssl=ctx,
         )
-        #    except (socket.gaierror, socket.herror) as ex:
-        #        pass
 
     async def send_message(self: Self, message: bytes) -> None:
         if not self.writer or self.writer.is_closing():
@@ -76,43 +77,43 @@ class DOTClient:
         return (await self.reader.read(4096))[2:]
 
 
-class DOTClientPool:
+class ClientConnectionPool:
     def __init__(
         self: Self,
-        max_clients: int = 10,
+        max_connections: int = 10,
         *args: Any,
         **kwargs: Any,
     ) -> None:
         self._pool = deque(
-            maxlen=max_clients,
-        )  # asyncio.Queue(max_clients)
-        for _ in range(max_clients):
-            client = DOTClient(*args, **kwargs)
-            self._pool.append(client)
+            iterable=(
+                ClientConnection(*args, **kwargs) for _ in range(max_connections)
+            ),
+            maxlen=max_connections,
+        )
 
     @asynccontextmanager
-    async def get_client(
+    async def get_connection(
         self: Self,
         timeout: float = 0.1,
-    ) -> AsyncIterator[DOTClient]:
-        client = await self._get_client_from_pool(timeout)
-        logging.debug("get client: 0x%X", id(client))
+    ) -> AsyncIterator[ClientConnection]:
+        c = await self._get_connection_from_pool(timeout)
+        logging.debug("get client connection: 0x%X", id(c))
         try:
-            yield client
+            yield c
         finally:
-            self.release_client(client)
+            self.release_connection(c)
 
-    async def _get_client_from_pool(
+    async def _get_connection_from_pool(
         self: Self,
         timeout: float,
-    ) -> DOTClient:
+    ) -> ClientConnection:
         while True:
             try:
                 return self._pool.pop()
             except IndexError:
                 await asyncio.sleep(timeout)
 
-    def release_client(self: Self, client: DOTClient) -> None:
+    def release_connection(self: Self, client: ClientConnection) -> None:
         """Returm client to the pool."""
         self._pool.append(client)
 
@@ -122,14 +123,14 @@ class DOTProxyProtocol(asyncio.DatagramProtocol):
         self: Self,
         remote_host: str | None = None,
         remote_port: int | None = None,
-        max_clients: int = 10,
+        max_connections: int = 10,
     ) -> None:
-        self.client_pool = DOTClientPool(
-            max_clients,
+        self.pool = ClientConnectionPool(
+            max_connections,
             remote_host,
             remote_port,
         )
-        self.sem = asyncio.Semaphore(max_clients)
+        self.sem = asyncio.Semaphore(max_connections)
         self.done = asyncio.get_event_loop().create_future()
 
     def connection_made(
@@ -151,12 +152,12 @@ class DOTProxyProtocol(asyncio.DatagramProtocol):
         data: bytes,
         addr: tuple[str, int],
     ) -> None:
-        async with self.sem, self.client_pool.get_client() as client:
-            await client.send_message(data)
-            message = await client.receive_message()
+        async with self.sem, self.pool.get_connection() as conn:
+            await conn.send_message(data)
+            message = await conn.receive_message()
             logging.debug(
                 "reply from %s#%d: %s",
-                *client.writer.get_extra_info("peername"),
+                *conn.writer.get_extra_info("peername"),
                 message.hex(" "),
             )
             self.transport.sendto(message, addr)
@@ -176,7 +177,7 @@ class NameSpace(argparse.Namespace):
     port: int
     remote_host: str | None
     remote_port: int | None
-    max_clients: int
+    max_connections: int
     verbose: int
 
 
@@ -203,8 +204,8 @@ def _parse_args(
     parser.add_argument("--remote-host", help="remote host", type=str)
     parser.add_argument("--remote-port", help="remote port", type=int)
     parser.add_argument(
-        "--max-clients",
-        help="max clients",
+        "--max-connections",
+        help="max client connections",
         default=10,
         type=int,
     )
@@ -228,6 +229,9 @@ def int_or_none(v: str | None) -> int | None:
 async def async_main(argv: Sequence[str] | None) -> None:
     parser, args = _parse_args(argv)
 
+    # if not args.remote_host:
+    #     logging.warning("")
+
     log_level = max(
         logging.DEBUG,
         logging.CRITICAL - args.verbose * logging.DEBUG,
@@ -241,7 +245,7 @@ async def async_main(argv: Sequence[str] | None) -> None:
         lambda: DOTProxyProtocol(
             remote_host=args.remote_host or getenv("DNS"),
             remote_port=args.remote_port or int_or_none(getenv("DNS_PORT")),
-            max_clients=args.max_clients,
+            max_connections=args.max_connections,
         ),
         local_addr=(args.host, args.port),
     )
